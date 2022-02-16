21c8db9e92f2980ca332a4440ade03dca397c6b8
Hardik Shah <47896958+HardikShah1234@users.noreply.github.com>
2022-02-16 15:56:54 +0100

    second commit

--- app/src/main/java/com/harry/fibonacciseries/MainActivity.kt
+++ app/src/main/java/com/harry/fibonacciseries/MainActivity.kt
@@ -3,15 +3,19 @@ package com.harry.fibonacciseries
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.harry.fibonacciseries.adapter.RecyclerViewAdapter
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
     private var mRecyclerView: RecyclerView? = null
-    private val recycler_view_items = 20 //No. of items added to recyclerview at a time
+    private val recycler_view_items = 100 //No. of items added to recyclerview at a time
     private val TAG = MainActivity::class.qualifiedName
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,15 +29,23 @@ class MainActivity : AppCompatActivity() {
         val recyclerViewAdapter = RecyclerViewAdapter(recycler_view_items);
         mRecyclerView!!.adapter = recyclerViewAdapter
 
-        mRecyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener(){
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-                Log.d(TAG, "last visible position: ${mLayoutManager.findLastCompletelyVisibleItemPosition()}, total count: ${mLayoutManager.itemCount}")
-                if (mLayoutManager.findLastCompletelyVisibleItemPosition() >= mLayoutManager.itemCount - 1) {
-                    recyclerViewAdapter.addItems(itemsAddCount = recycler_view_items)
-                }
+        lifecycleScope.launch(Dispatchers.Unconfined) {
+            withContext(Dispatchers.Default) {
+                mRecyclerView!!.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+                    override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                        super.onScrolled(recyclerView, dx, dy)
+                        Log.d(TAG, "last visible position: ${mLayoutManager.findLastCompletelyVisibleItemPosition()}, total count: ${mLayoutManager.itemCount}")
+                        if (mLayoutManager.findLastCompletelyVisibleItemPosition() >= mLayoutManager.itemCount - 1) {
+                            recyclerViewAdapter.addItems(itemsAddCount = recycler_view_items)
+                        }
+                    }
+                })
             }
-        })
+
+        }
+
+
+
 
     }
 }
